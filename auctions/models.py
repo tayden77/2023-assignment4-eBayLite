7bb621a4ad252f8ddb4131d7064e50eab6b4226a
Hjriebe <hjriebe@alaska.edu>
2023-10-18 14:07:13 -0800

    User model

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from PIL import Image
 
 
 class User(AbstractUser):
-    pass
+    username = models.CharField(unique=True, max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.profile_picture.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.profile_picture.path)

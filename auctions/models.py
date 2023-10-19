ee8b56b26f4f145f0a123e86e71730af75388728
Hjriebe <hjriebe@alaska.edu>
2023-10-18 16:37:06 -0800

    ok

--- auctions/models.py
+++ auctions/models.py
@@ -8,18 +8,4 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
     date_joined = models.DateTimeField(auto_now_add=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
-
-    def __str__(self):
-        return f'{self.username} Profile'
     
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.profile_picture.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.profile_picture.path)
-

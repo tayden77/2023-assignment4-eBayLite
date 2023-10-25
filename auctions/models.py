2916e997d9a753bb9e262b25b0e296f174b3ee81
Hjriebe <hjriebe@alaska.edu>
2023-10-25 12:45:42 -0800

    Added many models

--- auctions/models.py
+++ auctions/models.py
@@ -9,3 +9,35 @@ class User(AbstractUser):
     password = models.CharField(max_length=128)
     date_joined = models.DateTimeField(auto_now_add=True)
     
+
+class Listing(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)

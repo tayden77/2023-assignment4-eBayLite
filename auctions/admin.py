08cc0f4dff01dce318759095caa969b0b4c47d21
Hjriebe <hjriebe@alaska.edu>
2023-10-25 12:48:25 -0800

    register new models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Listing, Bid, Comment, Category, Watchlist
 
 class UserAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Watchlist)

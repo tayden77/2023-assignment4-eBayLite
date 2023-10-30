21163658191b0ba1fdd1a7bb47577969e4c5cc7d
Hjriebe <hjriebe@alaska.edu>
2023-10-30 12:01:25 -0800

    ok

--- auctions/context_processors.py
+++ auctions/context_processors.py
@@ -1,4 +1,4 @@
-# Additional context processor for loading all listing categories
+# Custom context processor for loading all listing categories
 
 def categories(request):
     from .models import Category
